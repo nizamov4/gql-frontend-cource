b1e13c69f5cefe25814839d7d6f61ed55272a99e
nizamov4 <ireknizamov94@gmail.com>
2022-10-21 19:47:16 +0300

    add file

--- gql-frontend-cource/src/app/bus/pet/counter.js
+++ gql-frontend-cource/src/app/bus/pet/counter.js
@@ -10,9 +10,23 @@ import { useQueryAvailablePets } from './hooks/useQueryAvailablePets';
 export const Counter = () => {
   const { loading, error, data } = useQueryAvailablePets();
 
+  if (loading) {
+    return  <p>Loading...</p>
+  }
+  if (error) {
+    return (
+      <p>
+        We have a problem: {error.message}
+      </p>
+    )
+  }
+
   console.log(data);
 
   return (
-    <p>Counter</p>
+    <p>
+      AvailablePets:
+      {data.availablePets}
+    </p>
   )
 };
