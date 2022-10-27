cafb5933cd4d595c05ce4251dc3fdac1159679db
nizamov4 <ireknizamov94@gmail.com>
2022-10-28 01:04:03 +0300

    add link for auth

--- gql-frontend-cource/src/app/init/client.js
+++ gql-frontend-cource/src/app/init/client.js
@@ -1,12 +1,25 @@
 //Core
 import { ApolloClient, InMemoryCache, HttpLink  } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 
-//GraphQl server 
+//GraphQl server
 const cache = new InMemoryCache();
 const uri = 'https://funded-pet-library.moonhighway.com/'
 const link = new HttpLink ({uri})
 
+//Auth
+const authLink = setContext ((_, {headers}) => {
+    const token = localStorage.getItem('token', )
+
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}`:'',
+        }
+    }
+})
+
 export const client = new ApolloClient({
     cache: cache,
-    link
+    link: authLink.concat(link)
 });
